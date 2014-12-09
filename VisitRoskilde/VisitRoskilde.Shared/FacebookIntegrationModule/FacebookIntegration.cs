adad292bbbb2e8159d62ac1798c75493b40e59a8
Morten Toudahl <toudahl@protonmail.ch>
2014-12-09 14:10:42 +0100

    Prepared the FacebookIntegration class for DataCollection

--- VisitRoskilde/VisitRoskilde.Shared/FacebookIntegrationModule/FacebookIntegration.cs
+++ VisitRoskilde/VisitRoskilde.Shared/FacebookIntegrationModule/FacebookIntegration.cs
@@ -1,26 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 using Windows.UI.Xaml.Controls;
 using VisitRoskilde.Interfaces;
 
 namespace VisitRoskilde.FacebookIntegrationModule
 {
-    class FacebookIntegration: ILoad
+    [DataContract]
+    class FacebookIntegration: ILoad, IDataCollectable
     {
-        // Load information from the Settings Object
+        private bool _status;
+        [DataMember]
+        private string _userHomeCity;
+        [DataMember]
+        private string _userGender;
+        [DataMember]
+        private int _userAge;
+
         public bool LoadData()
         {
             throw new NotImplementedException();
         }
 
-        public bool Status { get; set; }
+        public string UserHomeCity
+        {
+            get { return _userHomeCity; }
+            private set { _userHomeCity = value; }
+        }
 
+        public string UserGender
+        {
+            get { return _userGender; }
+            private set { _userGender = value; }
+        }
+
+        public int UserAge
+        {
+            get { return _userAge; }
+            private set { _userAge = value; }
+        }
+
+        
+
+        /// <summary>
+        /// Returns true if the user is logged in.
+        /// </summary>
+        public bool Status
+        {
+            get { return _status; }
+            private set { _status = value; }
+        }
+
+        /// <summary>
+        /// Log in using the provided credentials
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
         public void LogIn(string username, string password)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Logs out from the currently active user.
+        /// </summary>
         public void LogOut()
         {
             throw new NotImplementedException();
