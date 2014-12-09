5a872ec3bb72ffb125b74381568726a97e7f78d0
Morten Toudahl <toudahl@protonmail.ch>
2014-12-09 15:11:43 +0100

    DataCollection is kinda done.
    For now the data is not going anywhere.
    I have put in partial code for uploading the xml.

--- VisitRoskilde/VisitRoskilde.Shared/DataCollectionModule/DataDelivery.cs
+++ VisitRoskilde/VisitRoskilde.Shared/DataCollectionModule/DataDelivery.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Runtime.Serialization;
+using System.ServiceModel;
 using System.Text;
+using Windows.Storage;
 using VisitRoskilde.Interfaces;
 using VisitRoskilde.Persistence;
 
@@ -10,18 +14,37 @@ namespace VisitRoskilde.DataCollectionModule
     [DataContract]
     class DataDelivery : Serializer<DataDelivery>
     {
+        [DataMember]
+        private List<IDataCollectable> _collectables;
+
         public DataDelivery()
         {
             _fileName = "dataCollection.xml";
         }
-        public bool SaveData(List<IDataCollectable> collectedInformation)
+
+        public List<IDataCollectable> Collectables
         {
-            throw new NotImplementedException();
+            get { return _collectables; }
+            set { _collectables = value; }
+        }
+
+        private async void UploadData()
+        {
+            StorageFile storageFile = await ApplicationData.Current.LocalFolder.GetFileAsync(_fileName);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://f0cus.myds.me/visitRoskilde/datacollector.php");
+            request.Method = "POST";
+            Stream stream = await storageFile.OpenStreamForWriteAsync();
+            //AsyncCallback callback = new AsyncCallback();
+            //request.BeginGetRequestStream();
+            //HttpWebResponse resp = request.BeginGetResponse() as HttpWebResponse;
+            stream.Dispose();
+            // And all is happy with the uploaded data.
         }
 
         ~DataDelivery()
         {
             Serialize();
+            UploadData();
         }
     }
 }
