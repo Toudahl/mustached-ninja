c8b6d0cdfd80b05d6814d3ebc11d7271b6a3de52
Lalli-Oni <pill>
2014-12-10 14:18:34 +0100

    Added Facebook .NET NuGet 7.0.0 Alpha packages
    Added FacebookIntegration.LogIn() Method

--- VisitRoskilde/VisitRoskilde.Shared/FacebookIntegrationModule/FacebookIntegration.cs
+++ VisitRoskilde/VisitRoskilde.Shared/FacebookIntegrationModule/FacebookIntegration.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Runtime.Serialization;
 using System.Text;
+using Windows.Security.Authentication.Web;
+using Windows.UI.Popups;
 using Windows.UI.Xaml.Controls;
+using Facebook;
 using VisitRoskilde.Interfaces;
 
 namespace VisitRoskilde.FacebookIntegrationModule
@@ -10,6 +14,11 @@ namespace VisitRoskilde.FacebookIntegrationModule
     [DataContract]
     class FacebookIntegration: ILoad, IDataCollectable
     {
+        //AppID for testing app (localhost)
+        private string _fbAppIdTEST = "306430699547145";
+        //AppID for live app (https://f0cus.myds.me/visitRoskilde/)
+        private string _fbAppId = "304738753049673";
+        string _fbScope = "user_about_me,read_stream,publish_stream";
         private bool _status;
         [DataMember]
         private string _userHomeCity;
@@ -17,6 +26,7 @@ namespace VisitRoskilde.FacebookIntegrationModule
         private string _userGender;
         [DataMember]
         private int _userAge;
+        FacebookClient _fb = new FacebookClient();
 
         public bool LoadData()
         {
@@ -70,6 +80,77 @@ namespace VisitRoskilde.FacebookIntegrationModule
         {
             // If log out fails thrown an exception. Do not catch it.
             throw new NotImplementedException();
+        }
+
+        private async void LogIn()
+        {
+            var redirectUrl = "https://www.facebook.com/connect/login_success.html";
+            try
+            {
+                //fb.AppId = facebookAppId;
+                var loginUrl = _fb.GetLoginUrl(new
+                {
+                    client_id = _fbAppId,
+                    redirect_uri = redirectUrl,
+                    scope = _fbScope,
+                    display = "popup",
+                    response_type = "token"
+                });
+
+                var endUri = new Uri(redirectUrl);
+
+                WebAuthenticationResult WebAuthenticationResult = await WebAuthenticationBroker.AuthenticateAsync(
+                                                        WebAuthenticationOptions.None,
+                                                        loginUrl,
+                                                        endUri);
+                if (WebAuthenticationResult.ResponseStatus == WebAuthenticationStatus.Success)
+                {
+                    var callbackUri = new Uri(WebAuthenticationResult.ResponseData.ToString());
+                    var facebookOAuthResult = _fb.ParseOAuthCallbackUrl(callbackUri);
+                    var accessToken = facebookOAuthResult.AccessToken;
+                    if (String.IsNullOrEmpty(accessToken))
+                    {
+                        // User is not logged in, they may have canceled the login
+                    }
+                    else
+                    {
+                        // User is logged in and token was returned
+                        LoginSucceded(accessToken);
+                    }
+
+                }
+                else if (WebAuthenticationResult.ResponseStatus == WebAuthenticationStatus.ErrorHttp)
+                {
+                    throw new InvalidOperationException("HTTP Error returned by AuthenticateAsync() : " + WebAuthenticationResult.ResponseErrorDetail.ToString());
+                }
+                else
+                {
+                    // The user canceled the authentication
+                }
+            }
+            catch (Exception ex)
+            {
+                //
+                // Bad Parameter, SSL/TLS Errors and Network Unavailable errors are to be handled here.
+                //
+                throw ex;
+            }
+        }
+
+        private async void LoginSucceded(string accessToken)
+        {
+            dynamic parameters = new ExpandoObject();
+            parameters.access_token = accessToken;
+            parameters.fields = "id";
+
+            dynamic result = await _fb.GetTaskAsync("me", parameters);
+            parameters = new ExpandoObject();
+            parameters.id = result.id;
+            parameters.access_token = accessToken;
+
+            //TODO: Add something here to tell the user he is logged in
+            MessageDialog fbLogMDialog = new MessageDialog("You are logged into Facebook now.");
+
         }
     }
 }
