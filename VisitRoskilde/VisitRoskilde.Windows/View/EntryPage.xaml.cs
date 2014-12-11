6ea460ddda678026e6ca7f90dfe71c78894b10f9
Morten Toudahl <toudahl@protonmail.ch>
2014-12-11 23:19:00 +0100

    Two coloured triangles reacting to PointerEntered and PointerExited

--- VisitRoskilde/VisitRoskilde.Windows/View/EntryPage.xaml.cs
+++ VisitRoskilde/VisitRoskilde.Windows/View/EntryPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -14,41 +15,99 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using Windows.UI.Xaml.Shapes;
 
 namespace VisitRoskilde.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    
+    
+
     public sealed partial class EntryPage : Page
     {
+        private Polygon topTriangle;
+        private Polygon bottomTriangle;
+
+        
         public EntryPage()
         {
             this.InitializeComponent();
-            //double screenHeight = Window.Current.Bounds.Height;
-            //double screenWidth = Window.Current.Bounds.Width;
+            double screenHeight = Window.Current.Bounds.Height;
+            double screenWidth = Window.Current.Bounds.Width;
+         
+
+            double hypotenuse = Math.Sqrt(Math.Pow(screenHeight, 2) + Math.Pow(screenWidth, 2));
+
+            Canvas testCanvas = new Canvas();
+            testCanvas.Width = Math.Round(hypotenuse);
+
+            #region Top triangle
+            topTriangle = new Polygon();
+            Point topTrianglePoint1 = new Point(0, 0); // Top left
+            Point topTrianglePoint2 = new Point(screenWidth, 0); // Top right
+            Point topTrianglePoint3 = new Point(0, screenHeight);
 
 
-            //double diagonalPow2 = Math.Pow(screenHeight,2) + Math.Pow(screenWidth,2);
+            topTriangle.Points.Add(topTrianglePoint1);
+            topTriangle.Points.Add(topTrianglePoint2);
+            topTriangle.Points.Add(topTrianglePoint3);
+
+            topTriangle.Fill = new SolidColorBrush(Colors.DarkRed);
+
+            topTriangle.PointerEntered += TopTriangleOnPointerEntered;
+            topTriangle.PointerExited += TopTriangleOnPointerExited;
+
+            grid_main.Children.Add(topTriangle);
+            #endregion
 
 
 
-            //double hypotenuse = Math.Sqrt(diagonalPow2);
 
-            //Canvas testCanvas = new Canvas();
-            //testCanvas.Width = Math.Round(hypotenuse);
-            //text_debug.Text = Math.Pow(Math.Sin(hypotenuse / screenHeight),-1).ToString();
-
-            //Polygon testShape = new Polygon();
-            //Point testPoint = new Point(0,0);
-            //Point testPoint2 = new Point(0,screenWidth);
-            //Point testPoint3 = new Point(screenHeight,screenWidth);
+            #region Bottom triangle
+            bottomTriangle = new Polygon();
+            Point bottomTrianglePoint1 = new Point(screenWidth, screenHeight); // Bottom right
+            Point bottomTrianglePoint2 = new Point(screenWidth, 0); // Top right
+            Point bottomTrianglePoint3 = new Point(0, screenHeight);
 
 
-            //testShape.Points.Add(testPoint);
-            //testShape.Points.Add(testPoint2);
-            //testShape.Points.Add(testPoint3);
+            bottomTriangle.Points.Add(bottomTrianglePoint1);
+            bottomTriangle.Points.Add(bottomTrianglePoint2);
+            bottomTriangle.Points.Add(bottomTrianglePoint3);
 
+
+            bottomTriangle.Fill = new SolidColorBrush(Colors.DarkGreen);
+
+            bottomTriangle.PointerEntered += BottomTriangleOnPointerEntered;
+            bottomTriangle.PointerExited += BottomTriangleOnPointerExited;
+
+            grid_main.Children.Add(bottomTriangle);
+            
+
+            #endregion
+
+
+        }
+
+        private void TopTriangleOnPointerExited(object sender, PointerRoutedEventArgs pointerRoutedEventArgs)
+        {
+            topTriangle.Fill = new SolidColorBrush(Colors.DarkRed);
+        }
+
+        private void TopTriangleOnPointerEntered(object sender, PointerRoutedEventArgs pointerRoutedEventArgs)
+        {
+            topTriangle.Fill = new SolidColorBrush(Colors.Red);
+        }
+
+        private void BottomTriangleOnPointerExited(object sender, PointerRoutedEventArgs pointerRoutedEventArgs)
+        {
+            bottomTriangle.Fill = new SolidColorBrush(Colors.DarkGreen);
+        }
+
+        private void BottomTriangleOnPointerEntered(object sender, PointerRoutedEventArgs pointerRoutedEventArgs)
+        {
+            bottomTriangle.Fill = new SolidColorBrush(Colors.Green);
         }
     }
 }
