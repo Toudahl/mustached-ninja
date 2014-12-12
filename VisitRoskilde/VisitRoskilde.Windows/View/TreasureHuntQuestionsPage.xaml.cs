64afb26161e62ba336d684d38f672018c3cc93cc
jakub9 <jakub.krzysztof.czapski@gmail.com>
2014-12-12 12:01:48 +0100

    added working image name comparison on TakePicturePage, also added TreasureHuntQuestionsPage and create bindings between those

--- VisitRoskilde/VisitRoskilde.Windows/View/TreasureHuntQuestionsPage.xaml.cs
+++ VisitRoskilde/VisitRoskilde.Windows/View/TreasureHuntQuestionsPage.xaml.cs
@@ -20,16 +20,16 @@ namespace VisitRoskilde.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class TreasureHuntEntryPage : Page
+    public sealed partial class TreasureHuntQuestionsPage : Page
     {
-        public TreasureHuntEntryPage()
+        public TreasureHuntQuestionsPage()
         {
             this.InitializeComponent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof (TreasureHuntQuestionsPage));
+            this.Frame.Navigate(typeof (TakePicturePage));
         }
     }
 }
--- VisitRoskilde/VisitRoskilde.WindowsPhone/View/TreasureHuntQuestionsPage.xaml.cs
+++ VisitRoskilde/VisitRoskilde.WindowsPhone/View/TreasureHuntQuestionsPage.xaml.cs
@@ -13,23 +13,27 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
 namespace VisitRoskilde.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class TreasureHuntEntryPage : Page
+    public sealed partial class TreasureHuntQuestionsPage : Page
     {
-        public TreasureHuntEntryPage()
+        public TreasureHuntQuestionsPage()
         {
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            this.Frame.Navigate(typeof (TreasureHuntQuestionsPage));
         }
     }
 }
