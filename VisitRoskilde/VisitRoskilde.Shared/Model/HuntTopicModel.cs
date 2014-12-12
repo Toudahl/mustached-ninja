3174d4e58e16fe8c1eb26a124437168f1410bc9e
jakub9 <jakub.krzysztof.czapski@gmail.com>
2014-12-12 10:24:32 +0100

    created HuntTopicModel.cs and TreasureHuntEntryPage, added Vikings Treausre hunt to listview and created bindings

---
+++ VisitRoskilde/VisitRoskilde.Shared/Model/HuntTopicModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace VisitRoskilde.Model
+{
+    class HuntTopicModel
+    {
+        private string _theme;
+        private string _time;
+        private string _topic;
+        private string _reward;
+        private string _start;
+        private string _end;
+
+        public string End
+        {
+            get { return _end; }
+            set { _end = value; }
+        }
+
+        public string Start
+        {
+            get { return _start; }
+            set { _start = value; }
+        }
+
+        public string Reward
+        {
+            get { return _reward; }
+            set { _reward = value; }
+        }
+
+        public string Topic
+        {
+            get { return _topic; }
+            set { _topic = value; }
+        }
+
+        public string Time
+        {
+            get { return _time; }
+            set { _time = value; }
+        }
+
+        public string Theme
+        {
+            get { return _theme; }
+            set { _theme = value; }
+        }
+
+        public override string ToString()
+        {
+            return Topic.ToString();
+        }
+    }
+}
---
+++ VisitRoskilde/VisitRoskilde.Shared/ViewModel/TreasureHuntEntryPageViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using VisitRoskilde.Annotations;
+using VisitRoskilde.Model;
+
+namespace VisitRoskilde.ViewModel
+{
+    class TreasureHuntEntryPageViewModel:INotifyPropertyChanged
+    {
+        private ObservableCollection<HuntTopicModel> _huntList;
+        private HuntTopicModel _selectedHunt;
+
+        public HuntTopicModel SelectedHunt
+        {
+            get { return _selectedHunt; }
+            set { _selectedHunt = value; }
+        }
+
+        public TreasureHuntEntryPageViewModel()
+        {
+            HuntList=new ObservableCollection<HuntTopicModel>();
+            HuntTopicModel h1=new HuntTopicModel();
+            h1.Topic = "Vikings";
+            h1.Time = "2:30 hours";
+            h1.Theme = "Viking Oriented Hunt";
+            h1.Reward = "Get 120% off on viking ship museum";
+            h1.Start = "Outside Cathedral";
+            h1.End = "Viking Musem";
+            HuntList.Add(h1);
+
+
+        }
+
+        
+
+        public ObservableCollection<HuntTopicModel> HuntList
+        {
+            get { return _huntList; }
+            set
+            {
+                _huntList = value;
+                OnPropertyChanged("HuntList");
+            }
+        }
+        
+        
+        #region prop changed
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+    
+}
